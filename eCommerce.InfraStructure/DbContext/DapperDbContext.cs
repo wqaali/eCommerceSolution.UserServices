3ca9928b3c500da82054a86a0690e52a509b8ab2
Waqar <55060717+wqaali@users.noreply.github.com>
2025-10-08 13:09:29 +0300

    User Ms connnection with my sql

--- eCommerce.InfraStructure/DbContext/DapperDbContext.cs
+++ eCommerce.InfraStructure/DbContext/DapperDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,14 @@ namespace eCommerce.InfraStructure.DbContext
         {
             _configuration = configuration;
             string connectionStringTemplate=_configuration.GetConnectionString("DefaultConnection")!;
-            string connectionStringValues = connectionStringTemplate.Replace("$HOST_NAME", Environment.GetEnvironmentVariable("HOST_NAME")).
-            Replace("$DB_NAME", Environment.GetEnvironmentVariable("DB_NAME")).
-            Replace("$DB_USER", Environment.GetEnvironmentVariable("DB_USER")).
-            Replace("$DB_PASSWORD", Environment.GetEnvironmentVariable("DB_PASSWORD"));         
+            string connectionStringValues = connectionStringTemplate.Replace("$MYSQL_HOST", Environment.GetEnvironmentVariable("MYSQL_HOST")).
+            Replace("$MYSQL_DATABASE", Environment.GetEnvironmentVariable("MYSQL_DATABASE")).
+            Replace("$MYSQL_USER", Environment.GetEnvironmentVariable("MYSQL_USER")).
+            Replace("$MYSQL_PASSWORD", Environment.GetEnvironmentVariable("MYSQL_PASSWORD")).
+            Replace("$MYSQL_PORT", Environment.GetEnvironmentVariable("MYSQL_PORT"));         
           
             //string connectionString = _configuration.GetConnectionString("DefaultConnection")!;
-            _connection = new SqlConnection(connectionStringValues);
+            _connection = new MySqlConnection(connectionStringValues);
         }
         public IDbConnection DbConnection => _connection;
     }
